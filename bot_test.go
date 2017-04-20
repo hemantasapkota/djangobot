e67b523489faa1bb44c129a4f2a6cda35ffd24f0
Hemanta Sapkota <laex.pearl@gmail.com>
2017-04-21 09:38:56 +1000

    Test password change feature.

--- bot_test.go
+++ bot_test.go
@@ -1,16 +1,26 @@
 package djangobot
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestLogin(t *testing.T) {
 
 	// Before running this test, make sure to set your username and password below.
 
+	username := ""
+	password := ""
+
+	if username == "" && password == "" {
+		t.Log("Cannot execute test. Please supply username and password.")
+		return
+	}
+
 	// Disqus
 	bot := With("https://disqus.com/profile/login/").
 		ForHost("disqus.com").
-		SetUsername("").
-		SetPassword("")
+		SetUsername(username).
+		SetPassword(password)
 
 	if bot.Error != nil {
 		panic(bot.Error)
@@ -27,11 +37,34 @@ func TestLogin(t *testing.T) {
 		panic(err)
 	}
 
-	 sessionid := bot.Cookie("sessionid").Value
-	 if sessionid == "" {
-	 	t.Error("Authentication failed.")
-	 }
+	sessionid := bot.Cookie("sessionid").Value
+	if sessionid == "" {
+		t.Error("Authentication failed.")
+		return
+	}
 
-	 t.Log(sessionid)
+	t.Log("Authenticated with session id: ", sessionid)
+
+	// Add details to the form data
+	data := map[string]string{}
+
+	//data := map[string]string{
+	//	"email":        "",
+	//	"old_password": "",
+	//	"password":     "",
+	//	"username":     "",
+	//}
+
+	if len(data) == 0 {
+		t.Log("Cannot change password. Form data empty.")
+		return
+	}
+
+	_, body, _ := bot.Requester("PUT", "https://disqus.com/users/self/account/").
+		Client.
+		Send(data).
+		End()
+
+	t.Log(body)
 
 }
